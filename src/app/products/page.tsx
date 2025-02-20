3fc27d8ab1dc93a6c1febb5d5955e7aaedab4d0c
gogol54 <uniuffs@gmail.com>
2025-02-20 15:38:44 -0300

    config variables and add shadcn material

---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const ProductPage = () => {
+  return (
+    <div className="p-5 border border-red-500 rounded-xl">
+      <h1 className="text-red-500">products page</h1>
+      <Button>Test BTN</Button>
+      <Input placeholder="Bora tancar esse projeto!" />
+    </div>
+  );
+};
+
+export default ProductPage;
