f7d66950a8258ad919b75ce166e17dcde3b12bbc
gogol54 <uniuffs@gmail.com>
2025-02-23 19:08:54 -0300

    cart initial configs, add and sum products

--- src/app/[slug]/menu/[productId]/components/item-details.tsx
+++ src/app/[slug]/menu/[productId]/components/item-details.tsx
@@ -3,10 +3,12 @@
 import { formatCurrency } from "@/app/helpers/format-currency";
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { Prisma, Product, Restaurant } from "@prisma/client"
+import { Prisma } from "@prisma/client"
 import { ChefHatIcon, ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import Image from "next/image";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { CartContext } from "../../contexts/cart";
+import CartSheet from "../../components/cart-sheet";
 
 interface ProductDetailsProps {
   product: Prisma.ProductGetPayload<{
@@ -23,6 +25,7 @@ interface ProductDetailsProps {
 
 const ProductDetails = ({product}: ProductDetailsProps) => {
   const [quantity, setQuantity] = useState<number>(1)
+  const { toggleCart, addProduct } = useContext(CartContext)
   const handleDecreaseQuantity = () => {
     setQuantity((prev) => {
       if(prev === 1)
@@ -33,7 +36,16 @@ const ProductDetails = ({product}: ProductDetailsProps) => {
   const handleIncreaseQuantity = () => {
     setQuantity((prev) => prev + 1)
  }
+ const handleAddToCart = () => {
+  addProduct({
+    ...product,
+    quantity
+  })
+  toggleCart()
+ }
+
   return (
+  <>
     <div className="relative z-50 mt-[-1.5rem] overflow-hidden rounded-t-3xl p-5 bg-white flex-auto flex-col flex">
       {/*RESTAURANTE*/}
       <div className="flex-auto overflow-hidden">
@@ -99,9 +111,10 @@ const ProductDetails = ({product}: ProductDetailsProps) => {
           </div>
         </ScrollArea>
       </div>
-      <Button className="mt-6 w-full rounded-full">Adicionar à sacola</Button>
+      <Button className="mt-6 w-full rounded-full" onClick={handleAddToCart}>Adicionar à sacola</Button>
     </div>
-  )
+    <CartSheet />
+  </>)
 }
 
 export default ProductDetails
