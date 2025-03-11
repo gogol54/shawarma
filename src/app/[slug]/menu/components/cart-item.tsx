b7585af0abd120218b404ac5df942d58ff33ad9f
gogol54 <uniuffs@gmail.com>
2025-03-11 20:23:17 -0300

    fixed 'use client' tag

--- src/app/[slug]/menu/components/cart-item.tsx
+++ src/app/[slug]/menu/components/cart-item.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { ChevronLeftIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
 import Image from "next/image";
 import { useContext } from "react";
--- src/app/[slug]/menu/components/cart-sheet.tsx
+++ src/app/[slug]/menu/components/cart-sheet.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { useSearchParams } from "next/navigation";
 import { useContext, useState } from "react";
 
