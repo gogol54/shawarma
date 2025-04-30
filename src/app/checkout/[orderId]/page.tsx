9d75db74180c57576b850984b53457288b5c3963
gogol54 <uniuffs@gmail.com>
2025-04-30 18:27:20 -0300

    retry

--- src/app/checkout/[orderId]/page.tsx
+++ src/app/checkout/[orderId]/page.tsx
@@ -1,24 +1,22 @@
+
 import CheckoutPage from '@/components/CheckoutPage';
 import { db } from '@/lib/prisma';
+interface CheckoutProps {
+  params: Promise<{ orderId: string }>;
+}
 
-export default async function Checkout({ params }: { params: { orderId: string } }) {
-  const orderId = params.orderId;
+export default async function Checkout({ params }: CheckoutProps) {
+  const { orderId } = await params;
+  const order = await db.order.findUnique(
+    { 
+      where: { 
+        id: parseInt(orderId, 10) 
+      } 
+    });
 
-  const order = await db.order.findUnique({ 
-    where: { 
-      id: parseInt(orderId, 10) 
-    } 
-  });
-
-  if (!order || !order.preferenceId || order.total == null) {
-    return <div>Pedido não encontrado ou incompleto.</div>;
-  }
-
-  return (
-    <CheckoutPage
-      orderId={order.id.toString()}
-      preferenceId={order.preferenceId}
-      amount={order.total}
-    />
-  );
+    if (!order || !order.preferenceId || order.total == null) {
+      // Você pode mostrar um loading, redirecionar ou lançar erro
+      return <div>Pedido não encontrado ou incompleto.</div>;
+    }
+  return <CheckoutPage orderId={order?.id.toString()} preferenceId={order?.preferenceId} amount={order?.total} />;
 }
